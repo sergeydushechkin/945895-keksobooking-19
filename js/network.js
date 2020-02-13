02a835b47de7265ab4e193be5c9104383fa50143
Sergey <dushechkin_serge@mail.ru>
2020-02-13 18:00:43 +0300

    Добавил функцию для отправки формы

--- js/network.js
+++ js/network.js
@@ -2,6 +2,7 @@
 
 (function () {
 
+  var URL_OFFER_SEND = 'https://js.dump.academy/keksobooking';
   var STATUS_OK = 200;
 
   var load = function (url, onLoad, onError) {
@@ -29,8 +30,34 @@
     xhr.send();
   };
 
+  var sendOffer = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000;
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS_OK) {
+        onLoad(xhr.response);
+      } else {
+        throw new Error('Ошибка отправки данных:' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения при отправке данных: ' + xhr.status + ' ' + xhr.statusText);
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос на отправку не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('POST', URL_OFFER_SEND);
+    xhr.send(data);
+  };
+
   window.network = {
-    load: load
+    load: load,
+    sendOffer: sendOffer
   };
 
 })();
