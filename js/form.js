46cf03506cdcc9f6734e8211c568db12ce76f4e0
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2020-02-26 21:48:39 +0300

    Добавил загрузку изображений

--- js/form.js
+++ js/form.js
@@ -23,6 +23,11 @@
 
   var mainPin = document.querySelector('.map__pin--main');
 
+  var avatarImage = document.querySelector('.ad-form-header__preview img');
+  var avatarInput = document.querySelector('.ad-form__field input[type=file]');
+  var housePhoto = document.querySelector('.ad-form__photo');
+  var houseUpload = document.querySelector('.ad-form__upload input[type=file]');
+
   // Для проверки состояния страницы
   var pageActive = false;
 
@@ -131,6 +136,8 @@
     selectType.addEventListener('change', onTypeChange);
     inputPrice.addEventListener('input', onPriceInput);
     selectTime.addEventListener('change', onTimeChange);
+    avatarInput.addEventListener('change', onAvatarInputChange);
+    houseUpload.addEventListener('change', onHouseUploadChange);
   };
 
   // Удаляет обработчики формы
@@ -143,6 +150,8 @@
     selectType.removeEventListener('change', onTypeChange);
     inputPrice.removeEventListener('input', onPriceInput);
     selectTime.removeEventListener('change', onTimeChange);
+    avatarInput.removeEventListener('change', onAvatarInputChange);
+    houseUpload.removeEventListener('change', onHouseUploadChange);
   };
 
   /* -------------------------Обработчики------------------------- */
@@ -204,6 +213,17 @@
     window.network.send(new FormData(adForm), onAdFormSubmitSuccess, onAdFormSubmitError);
     evt.preventDefault();
   };
+
+  // При изменении инпута с фотографией жилья
+  var onHouseUploadChange = function () {
+    window.images.loadImage(houseUpload, housePhoto);
+  };
+
+  // При изменении инпута с аватаром
+  var onAvatarInputChange = function () {
+    window.images.loadImage(avatarInput, avatarImage);
+  };
+
 
   /* -------------------------Экспорт------------------------- */
 
---
+++ js/images.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  // Устанавливает изображение в виде фона элемента
+  var setBackground = function (element, imageData) {
+    element.style.backgroundSize = 'cover';
+    element.style.backgroundImage = 'url(' + imageData + ')';
+  };
+
+  // Загружает выбранное изображение в элемент
+  var loadImage = function (fileInput, imageElement) {
+    var filename = fileInput.files[0].name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (extension) {
+      return filename.endsWith(extension);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        if (imageElement.tagName === 'IMG') {
+          imageElement.src = reader.result;
+        } else {
+          setBackground(imageElement, reader.result);
+        }
+      });
+
+      reader.readAsDataURL(fileInput.files[0]);
+    }
+  };
+
+  /* ---------------Экспорт--------------- */
+
+  window.images = {
+    loadImage: loadImage
+  };
+
+})();
