404e81fda8ac8c322477331772a04b888cd063bd
Sergey <dushechkin_serge@mail.ru>
2020-03-04 17:26:13 +0300

    Заменил for на forEach - Д26

--- js/util.js
+++ js/util.js
@@ -8,9 +8,9 @@
 
   // Переключить состояние набору элементов
   var setElementsState = function (elements, state) {
-    for (var elementIndex = 0; elementIndex < elements.length; elementIndex++) {
-      elements[elementIndex].disabled = state;
-    }
+    Array.from(elements).forEach(function (element) {
+      element.disabled = state;
+    });
   };
 
   // Показ сообщений
